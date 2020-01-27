93eb6575254e3dbf14478fd01f8f9e704bddf918
yuanping <3209605462@qq.com>
2020-01-27 15:32:04 +0800

    Initial Commit

---
+++ MusicList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicList
+//
+//  Created by 袁平 on 2020/1/27.
+//  Copyright © 2020 袁平. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MusicList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicList
+//
+//  Created by 袁平 on 2020/1/27.
+//  Copyright © 2020 袁平. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
